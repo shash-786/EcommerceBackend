28e0b54d64c04ce2e3e7061b105dacff8166d835
shash-786 <ssalian60@yahoo.com>
2024-04-14 10:13:13 +0530

    Controllers/cart Features Implemented, ran mod tidy, and some cleanup elsewhere

--- database/databasesetup.go
+++ database/databasesetup.go
@@ -2,8 +2,8 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
-  "fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -11,32 +11,31 @@ import (
 )
 
 func DBSet() *mongo.Client {
-  client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017")
-  if err != nil {
-    log.Fatal(err)
-  }
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-  ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017"))
+	if err != nil {
+		log.Fatalf("Error Connecting to the MongoDB: %v", err)
+	}
 
-  err = client.Connect(ctx)
-  if err != nil {
-    log.Fatal(err)
-  }
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatalf("Error Pinging MongoDB: %v", err)
+	}
 
-  err = client.Ping(context.TODO(), nil)
-  if err != nil {
-    log.Println("Failed to connect to the DB")
-    return
-  }
-
-  fmt.Prinprintln("Successfullly Connected to the client")
-  return client
+	fmt.Println("Successfullly Connected to the client")
+	return client
 }
 
 var Client = DBSet()
 
 func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("Ecommerce").Collection(collectionName)
+	return collection
 }
 
-func ProductData(clien *mongo.Client, collectionName string) *mongo.Collection {
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("Ecommerce").Collection(collectionName)
+	return collection
 }
